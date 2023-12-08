4f68b307828b2b397cc067b8aecac4874bf3212e
NpS <preeyanan.nps@gmail.com>
2023-12-08 16:43:18 +0700

    7.3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+if(x =='A')
+{
+    return 'Z';
+}else if(x<='Z'&& x>'A')
+{
+     return x-1;
 }
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+else
+{
+    return 48;
+}
 }
